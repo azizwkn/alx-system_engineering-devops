df40563d9647a9f7619e128f82ccf7fcf4123ed9
Aziz <ngwk.zz@outlook.com>
2023-12-05 12:39:44 +0300

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,42 +1,20 @@
 #!/usr/bin/env bash
+# automate the task of creating a custom HTTP header response
 # configures a brand new Ubuntu machine to the requirements:
 # the name of the custom HTTP header must be X-Served-By
-# the value of the HTTP header is the hostname of the running server.
+# the value of the HTTP header is the hostname of the running server
 
-# Update package list
-exec { 'apt_update':
-  command => 'apt-get update',
-  path    => '/usr/bin',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-# Install Nginx
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt_update'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Configure Nginx with custom HTTP response header
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        # Add custom HTTP response header
-        add_header X-Served-By ${::hostname};
-    }
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable Nginx to start on boot
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
