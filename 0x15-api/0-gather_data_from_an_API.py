031385b23aaaa971885fb5686e61eb8ed1e82957
Aziz <ngwk.zz@outlook.com>
2024-01-03 07:32:21 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,31 +9,10 @@ import sys
 
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    # Construct the URL for the user API endpoint using the user ID
-    # from the command-line argument
-    user_id = sys.argv[1]
-    user_url = base_url + "users/{}".format(user_id)
-
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-
-    # Construct the URL for the todos API endpoint with a query parameter
-    # for the user ID
-    todos_url = base_url + "todos"
-    todos_params = {"userId": user_id}
-
-    todos_response = requests.get(todos_url, params=todos_params)
-    todos_data = todos_response.json()
-
-    # Filter completed tasks and retrieve their titles
-    completed_tasks = [task.get("title") for task in todos_data
-                       if task.get("completed")]
-
-    # Display information about completed tasks
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get("name"), len(completed_tasks), len(todos_data)))
-
-    # Display the titles of completed tasks
-    for task_title in completed_tasks:
-        print("\t {}".format(task_title))
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
