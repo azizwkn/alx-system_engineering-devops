3a5a4fa178f53ef11d514b5447205efe3b420956
Aziz <ngwk.zz@outlook.com>
2024-01-18 08:37:14 +0300

    Return number of subreddits

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Queries Reddit API"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return number of subscribers"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+                "Uer-Agent": "Firefox/0.0"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        try:
+            result = response.json().get("data")
+            return result.get("subscribers")
+
+        except KeyError:
+            return 0
+    else:
+        return 0
